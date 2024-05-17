b82d2eb7d66ff0129591d8c657026a36442adc49
raihank43 <raihankusuma5@gmail.com>
2024-05-18 00:28:19 +0700

    feat: testing crud using nestjs and prisma

--- question_2/document-management-system/src/app.module.ts
+++ question_2/document-management-system/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService, UserService, PostService],
 })
 export class AppModule {}
