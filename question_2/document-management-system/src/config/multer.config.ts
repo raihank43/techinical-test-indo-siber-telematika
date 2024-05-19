84c35f77753217f281f7305453d211758b52c265
raihank43 <raihankusuma5@gmail.com>
2024-05-19 16:47:27 +0700

    feat: test upload to cloudinary using multer memory storage

--- question_2/document-management-system/src/config/multer.config.ts
+++ question_2/document-management-system/src/config/multer.config.ts
@@ -1,15 +1,6 @@
 import { diskStorage } from 'multer';
 import * as path from 'path';
+import * as multer from 'multer';
 export const multerConfig = {
-  storage: diskStorage({
-    // destination: './document',
-    filename: (req, file, cb) => {
-      console.log(file, 'file from multer config');
-      const filename: string =
-        path.parse(file.originalname).name.replace(/\s/g, '') + Date.now();
-      const extension: string = path.parse(file.originalname).ext;
-
-      cb(null, `${filename}${extension}`);
-    },
-  }),
+  storage: multer.memoryStorage(),
 };
--- question_2/document-management-system/src/document/document.controller.ts
+++ question_2/document-management-system/src/document/document.controller.ts
@@ -10,6 +10,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { multerConfig } from 'src/config/multer.config';
 import * as cloudinary from 'cloudinary';
+import * as mime from 'mime-types';
 
 @Controller()
 export class DocumentController {
@@ -26,8 +27,24 @@ export class DocumentController {
   @UseInterceptors(FileInterceptor('file', multerConfig))
   async uploadDocument(@UploadedFile() file): Promise<any> {
     console.log(file, 'file');
-    const result = await cloudinary.v2.uploader.upload(file.path);
-    console.log(result, 'result');
-    return result;
+    const fileName = file.originalname;
+    const mimeType = file.mimetype;
+    const data = Buffer.from(file.buffer).toString('base64');
+    const dataURI = `data:${mimeType};base64,${data}`;
+
+    // generate randomName for file
+    const randomName =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+
+    // Get the extension from the mimetype
+    const extensionName = mime.extension(mimeType).split(' ')[0];
+    const result = await cloudinary.v2.uploader.upload(dataURI, {
+      folder: 'document-management-system',
+      resource_type: 'auto',
+      public_id: randomName,
+      format: extensionName ? extensionName : null, // Check if extension is a string before using it
+    });
+    return { ...result, message: 'Document uploaded successfully' };
   }
 }
