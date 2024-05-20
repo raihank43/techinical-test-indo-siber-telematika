71496414b4fe016dd87c06d79b3423e2cd40fba4
raihank43 <raihankusuma5@gmail.com>
2024-05-20 23:24:58 +0700

    feat: wiring user document

---
+++ question_2/client/src/interfaces/document-interface.ts
@@ -0,0 +1,9 @@
+export interface IDocument {
+  id: number;
+  title: string;
+  documentUrl: string;
+  docType: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+}
--- question_2/client/src/views/Home.tsx
+++ question_2/client/src/views/Home.tsx
@@ -1,69 +1,119 @@
 import { Button } from "@/components/ui/button";
+import { IDocument } from "@/interfaces/document-interface";
+import { instance } from "@/utils/axios";
+import { useEffect, useState } from "react";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-
 export default function Home() {
+  const [documents, setDocuments] = useState<IDocument[]>([]);
+  const fetchUserDocuments = async () => {
+    try {
+      const response = await instance.get("/document", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.status === 200) {
+        // Set the documents state with the response data
+        // setDocuments(response.data);
+        setDocuments(response.data);
+      }
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDocuments();
+  }, []);
+
+  console.log(documents, "<<<<<<<<<<<<<<");
+
   return (
     <div className="p-6">
       <div className="flex flex-col rounded-lg shadow-lg">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr className="bg-slate-100">
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
-                    >
-                      Number
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
-                    >
-                      File Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
-                    >
-                      Date Created
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
-                    >
-                      Type
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-center text-xs font-medium text-yellow-600 uppercase tracking-wider"
-                    >
-                      Action
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">1</td>
-                    <td className="px-6 py-4 whitespace-nowrap">file1.txt</td>
-                    <td className="px-6 py-4 whitespace-nowrap">2021-09-01</td>
-                    <td className="px-6 py-4 whitespace-nowrap">Text</td>
-                    <td className="flex px-6 py-4 whitespace-nowrap gap-2 justify-center">
-                      <Button className="text-white flex gap-2  hover:bg-blue-800 bg-blue-600">
-                        <FaCloudDownloadAlt />
-                        Download
-                      </Button>
-                      <Button className="text-white flex gap-2 hover:bg-red-700 bg-red-600">
-                        <MdDelete />
-                        Delete
-                      </Button>
-                    </td>
-                  </tr>
-                  {/* Repeat for other rows */}
-                </tbody>
-              </table>
+              <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                      <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50">
+                          <tr className="bg-slate-100">
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
+                            >
+                              Number
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
+                            >
+                              File Name
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
+                            >
+                              Date Created
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-yellow-600 uppercase tracking-wider"
+                            >
+                              Type
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-center text-xs font-medium text-yellow-600 uppercase tracking-wider"
+                            >
+                              Action
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                          {documents.map((document, index) => {
+                            return (
+                              <tr key={index}>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                  {index + 1}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                  {document.title}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                  {document.createdAt}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                  {document.docType}
+                                </td>
+                                <td className="flex px-6 py-4 whitespace-nowrap gap-4 justify-center">
+                                  <Button
+                                    onClick={() =>
+                                      window.open(document.documentUrl)
+                                    }
+                                    className="text-white flex gap-2  hover:bg-blue-800 bg-blue-600"
+                                  >
+                                    <FaCloudDownloadAlt />
+                                    Download
+                                  </Button>
+                                  <Button className="text-white flex gap-2 hover:bg-red-700 bg-red-600">
+                                    <MdDelete />
+                                    Delete
+                                  </Button>
+                                </td>
+                              </tr>
+                            );
+                          })}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
